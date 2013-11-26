9b752c9bea9fb9036ddee18ba663ac164a3e85d3
John McCrae <john@mccr.ae>
2013-11-26 11:13:45 -0500

    Refactor to Java 6

--- scripts/opensub/filter-docs.scala
+++ scripts/opensub/filter-docs.scala
@@ -1,5 +1,4 @@
 import java.io.File
-import java.nio.file.Files
 
 val docLine = ".* fromDoc=\"(.*)\" toDoc=\"(.*)\".*".r
 
@@ -8,14 +7,16 @@ new File("OpenSubtitles2012/filtered").mkdir
 val rawLoc = "OpenSubtitles2012/raw/"
 val fltLoc = "OpenSubtitles2012/filtered/"
 
+def cp(f1 : String, f2 : String) = Runtime.getRuntime().exec("cp %s %s" format(f1,f2)).waitFor()
+
 io.Source.stdin.getLines.foreach {
     case docLine(from,to) => {
         System.err.println(from + " // " + to)
         new File(fltLoc + from).getParentFile().mkdirs
         new File(fltLoc + to).getParentFile().mkdirs
-        Files.copy(new File(rawLoc + from).toPath(),
+        cp(new File(rawLoc + from).toPath(),
                    new File(fltLoc + from).toPath())
-        Files.copy(new File(rawLoc + to).toPath(),
+        cp(new File(rawLoc + to).toPath(),
                    new File(fltLoc + to).toPath())
     }
     case _ => 
